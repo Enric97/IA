c861e418c067e06ae35e506795ecf1a560716cbf
Enric97 <enricvima97@gmail.com>
2022-05-21 17:55:26 +0200

    Update 2022_05_21_CRigat_EVinas_Practica1.py

--- P1/2022_05_21_CRigat_EVinas_Practica1.py
+++ P1/2022_05_21_CRigat_EVinas_Practica1.py
@@ -1,11 +1,12 @@
 
 
+
 # Clase del Node
 class Node:
     def __init__(self, name):
         self.childs = {}
         self.name = name
-        self.leaf = False
+        self.isVisited = False
 
     def __str__(self):
         return str(self.name)
@@ -14,8 +15,6 @@ class Node:
         return str(self.name)
 
 # Relacionem els nodes i assignem valors
-
-
 def createTree():
 
     Cuina.childs.update({nA1: 1, nA3: 1.5})
@@ -23,15 +22,16 @@ def createTree():
     nA2.childs.update({nA1: 1.75, nA4: 0.5})
     nA3.childs.update({Cuina: 1.5, nA1: 0.5, nA4: 1.75, nE3: 2.5, nE4: 2})
     nA4.childs.update({nA3: 1.75, nA2: 0.5, nB3: 2,
-                      nB4: 2.5, nD3: 2.5, nD4: 3})
+                      nB4: 2.5, nD3: 3.5, nD4: 3})
 
     nB1.childs.update({nB3: 1.75, nB2: 0.5})
-    nB2.childs.update({nB4: 17.5, nB1: 0.5})
+    nB2.childs.update({nB4: 1.75, nB1: 0.5})
     nB3.childs.update({nA4: 2, nB1: 1.75, nB4: 0.5, nD4: 2.5})
     nB4.childs.update({nB2: 1.75, nB3: 0.5, nC4: 1.5})
 
     nC1.childs.update({nC2: 1, nC4: 1.75})
-    nC2.childs.update({nC1: 1, nC3: 1, nC6: 0}) # a C6 no hi ha numero, suposem que es 0 ??
+    # a C6 no hi ha numero, suposem que es 0 ??
+    nC2.childs.update({nC1: 1, nC3: 1, nC6: 0})
     nC3.childs.update({nC2: 1, nC8: 1.75})
     nC4.childs.update({nB4: 1.5, nC1: 1.75, nC7: 0.5})
     nC5.childs.update({nC6: 0.5, nC8: 0.5})
@@ -42,7 +42,7 @@ def createTree():
     nD1.childs.update({nD2: 0.5, nD3: 1.75})
     nD2.childs.update({nD1: 0.5, nD4: 1.75})
     nD3.childs.update({nA4: 3.5, nC8: 1.5, nD1: 1.75, nD4: 0.5})
-    nD4.childs.update({nA4: 3, nB3: 2.5, nD2: 1.75, nD3: 1.75})
+    nD4.childs.update({nA4: 3, nB3: 2.5, nD2: 1.75, nD3: 0.5})
 
     nE1.childs.update({nE2: 0.5, nE3: 1.75})
     nE2.childs.update({nE1: 0.5, nE4: 1.75})
@@ -51,35 +51,19 @@ def createTree():
 
 
 def checkValidity(node):
-    # Comprovar si el node arriba fins a mataro o es mataro
-    # Si no pot arribar, retornara fals (no passa mai en el nostre cas)
-    result = []
-    print("Studing Node: ", node.name)
-    if(node == desti):
+
+    options = {}
+
+    if(node==desti):
         return True
 
-    for child in node.childs:
+    for child, cost in node.childs:
 
-        if(child == desti):
-            # print("\t", child.name, " is ", desti.name)
-            print("\t Reached ", desti, " !!!")
-            return True
-        if(child.leaf):
-            print("\t", child.name, " doesn't lead to ", desti.name)
-            return False
-        else:
-            print("\t Next child: ", child.name)
-            result.append(checkValidity(child))
-
-    # Si existeis un sol cami que sigui possible, retornem True, sino retornem False
-    if True in result:
-        return True
-    print("Not existing path across ", node)
-    return False
-
-# Basicament mirem el diccionari on guardem nodes i el seu cost
-# i agafem el que te un cost menor
-# IMPORTANT: es fa despres de comprovar que es viable arribar al desti
+        if(not node.isVisited):
+            node.isVisited=True
+            cost_a += cost
+            checkValidity(child)
+        
 
 
 def selectLessCost(options):
@@ -132,7 +116,7 @@ def Greedy():
 
 
 # def NodeCreation(list):
-
+  
 Cuina = Node("Cuina")
 
 nA1 = Node("A1")
@@ -164,24 +148,30 @@ nE2 = Node("E2")
 nE3 = Node("E3")
 nE4 = Node("E4")
 
+# list.extend((Cuina, nA1, nA2, nA3, nA4,
+# nB1, nB2, nB3, nB4,
+# nC1, nC2, nC3, nC4, nC5, nC6, nC7, nC8,
+# nD1, nD2, nD3, nD4,
+# nE1, nE2, nE3, nE4))
+
+
 # Variables globals
 
-nodes = []
-# actual = moia
-# desti = mataro
-# cost_a = 0
-# possiblePath = {}
-# finalPath = []
-# finalPath.append(actual)
 
+actual = Cuina
+desti = nA4
+cost_a = 0
+possiblePath = {}
+finalPath = []
+finalPath.append(actual)
 
-
+createTree()
 
 # print("Estudiem el recorregut entre ", actual.name, " i ", desti.name, "\n")
 
 # createTree()
 
-# Greedy()
+Greedy()
 
 # print("---------------------------------------\n")
 # print("El recorregut final (amb algoritme Greedy) passa per: ", finalPath, "\n")
